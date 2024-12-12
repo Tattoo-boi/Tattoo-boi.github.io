d91abdf8f24c57ae4706bad0ecf4d05a4e540f4f
Tattoo-boi <2001kaungkaung@gmail.com>
2024-12-12 01:47:42 -0500

    Update sketch.js

--- AlienSubwayProject/sketch.js
+++ AlienSubwayProject/sketch.js
@@ -1,12 +1,18 @@
-// p5.js animation for aliens
 function setup() {
     let canvas = createCanvas(windowWidth, 300);
-    canvas.parent('alienCanvas');
-    background(0);
+    canvas.parent("hero");
+    frameRate(30);
 }
 
 function draw() {
+    background(0);
+    fill(255);
     noStroke();
-    fill(random(100, 255), random(100, 255), random(100, 255));
-    ellipse(random(width), random(height), random(10, 50));
+    let x = frameCount % width;
+
+    // Subway animation (moving rectangle)
+    rect(x, height / 2 - 50, 100, 100);
+    fill(255, 0, 0);
+    textSize(20);
+    text("Subway Train Passing", x + 10, height / 2 - 60);
 }
